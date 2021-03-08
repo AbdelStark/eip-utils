f1643b0b6b5e04fae6a2586ae2d4c8fbe98faccd
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-03-08 11:22:54 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 pub mod eip1559{
+    use num_bigint::BigUint;
+    use std::cmp::Ordering;
+    use num_traits::{Zero, One};
+
     
     pub fn new() -> Service{
         Service{}
@@ -9,8 +13,20 @@ pub mod eip1559{
     }
 
     impl Service {
-        pub fn compute_base_fee(&self, parent_base_fee: &u64, parent_gas_used: &u64, parent_target_gas_used: &u64) -> u64{
-            0
+        pub fn compute_base_fee(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
+            match  parent_gas_used.cmp(&parent_target_gas_used) {
+              Ordering::Equal => parent_base_fee.clone(),
+              Ordering::Greater => self.compute_base_fee_asc(parent_base_fee, parent_gas_used, parent_target_gas_used),
+              Ordering::Less => self.compute_base_fee_desc(parent_base_fee, parent_gas_used, parent_target_gas_used)
+            }
+        }
+
+        fn compute_base_fee_asc(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
+            Zero::zero()
+        }
+
+        fn compute_base_fee_desc(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
+            Zero::zero()
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use eip_utils::eip1559;
+use num_bigint::BigUint;
 
 
 fn main() {
     let eip1559_service = &eip1559::new();
-    let parent_base_fee: u64 = 0;
-    let parent_gas_used: u64 = 0;
-    let parent_target_gas_used: u64 = 0;
+    let parent_base_fee: u64 = 1049238967;
+    let parent_gas_used: u64  = 10000000;
+    let parent_target_gas_used: u64 = 9000000;
+    let parent_base_fee = BigUint::from(parent_base_fee);
+    let parent_gas_used  = BigUint::from(parent_gas_used);
+    let parent_target_gas_used = BigUint::from(parent_target_gas_used);
     let base_fee = eip1559_service.compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
     println!("base fee: {}", &base_fee);
 }
