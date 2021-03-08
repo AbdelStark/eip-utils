75cc1cbc900f331fa383243439a3ddea4ed16a22
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-03-08 11:35:56 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod eip1559{
     use num_bigint::BigUint;
     use std::cmp::Ordering;
-    use num_traits::{Zero, One};
 
+    const BASEFEE_MAX_CHANGE_DENOMINATOR: u64  = 8;
     
     pub fn new() -> Service{
         Service{}
@@ -16,17 +16,21 @@ pub mod eip1559{
         pub fn compute_base_fee(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
             match  parent_gas_used.cmp(&parent_target_gas_used) {
               Ordering::Equal => parent_base_fee.clone(),
-              Ordering::Greater => self.compute_base_fee_asc(parent_base_fee, parent_gas_used, parent_target_gas_used),
-              Ordering::Less => self.compute_base_fee_desc(parent_base_fee, parent_gas_used, parent_target_gas_used)
+              Ordering::Greater => self.compute_base_fee_increase(parent_base_fee, parent_gas_used, parent_target_gas_used),
+              Ordering::Less => self.compute_base_fee_decrease(parent_base_fee, parent_gas_used, parent_target_gas_used)
             }
         }
 
-        fn compute_base_fee_asc(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
-            Zero::zero()
+        fn compute_base_fee_increase(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
+            let gas_delta = parent_gas_used - parent_target_gas_used;
+            let fee_delta = (parent_base_fee * gas_delta) / parent_target_gas_used / BASEFEE_MAX_CHANGE_DENOMINATOR;
+            parent_base_fee + fee_delta
         }
 
-        fn compute_base_fee_desc(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
-            Zero::zero()
+        fn compute_base_fee_decrease(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
+            let gas_delta = parent_target_gas_used - parent_gas_used;
+            let fee_delta = (parent_base_fee * gas_delta) / parent_target_gas_used / BASEFEE_MAX_CHANGE_DENOMINATOR;
+            parent_base_fee - fee_delta        
         }
     }
 
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use num_bigint::BigUint;
 
 fn main() {
     let eip1559_service = &eip1559::new();
-    let parent_base_fee: u64 = 1049238967;
-    let parent_gas_used: u64  = 10000000;
-    let parent_target_gas_used: u64 = 9000000;
+    let parent_base_fee: u64 = 1000000000;
+    let parent_gas_used: u64  = 7000000;
+    let parent_target_gas_used: u64 = 6000000;
     let parent_base_fee = BigUint::from(parent_base_fee);
     let parent_gas_used  = BigUint::from(parent_gas_used);
     let parent_target_gas_used = BigUint::from(parent_target_gas_used);
