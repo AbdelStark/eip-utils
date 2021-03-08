de20d8d29c0e731a42fb9271c63b2e728ced5253
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-03-08 14:06:35 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,47 @@
-pub mod eip1559{
+pub mod eip1559 {
     use num_bigint::BigUint;
-    use std::cmp::Ordering;
+    use num_traits::One;
+    use std::cmp::{self, Ordering};
 
-    const BASEFEE_MAX_CHANGE_DENOMINATOR: u64  = 8;
-    
-    pub fn new() -> Service{
-        Service{}
-    }
+    const BASEFEE_MAX_CHANGE_DENOMINATOR: u64 = 8;
 
-    pub struct Service{
-
-    }
-
-    impl Service {
-        pub fn compute_base_fee(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
-            match  parent_gas_used.cmp(&parent_target_gas_used) {
-              Ordering::Equal => parent_base_fee.clone(),
-              Ordering::Greater => self.compute_base_fee_increase(parent_base_fee, parent_gas_used, parent_target_gas_used),
-              Ordering::Less => self.compute_base_fee_decrease(parent_base_fee, parent_gas_used, parent_target_gas_used)
+    pub fn compute_base_fee(
+        parent_base_fee: &BigUint,
+        parent_gas_used: &BigUint,
+        parent_target_gas_used: &BigUint,
+    ) -> BigUint {
+        match parent_gas_used.cmp(&parent_target_gas_used) {
+            Ordering::Equal => parent_base_fee.clone(),
+            Ordering::Greater => {
+                compute_base_fee_increase(parent_base_fee, parent_gas_used, parent_target_gas_used)
+            }
+            Ordering::Less => {
+                compute_base_fee_decrease(parent_base_fee, parent_gas_used, parent_target_gas_used)
             }
         }
-
-        fn compute_base_fee_increase(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
-            let gas_delta = parent_gas_used - parent_target_gas_used;
-            let fee_delta = (parent_base_fee * gas_delta) / parent_target_gas_used / BASEFEE_MAX_CHANGE_DENOMINATOR;
-            parent_base_fee + fee_delta
-        }
-
-        fn compute_base_fee_decrease(&self, parent_base_fee: &BigUint, parent_gas_used: &BigUint, parent_target_gas_used: &BigUint) -> BigUint{
-            let gas_delta = parent_target_gas_used - parent_gas_used;
-            let fee_delta = (parent_base_fee * gas_delta) / parent_target_gas_used / BASEFEE_MAX_CHANGE_DENOMINATOR;
-            parent_base_fee - fee_delta        
-        }
     }
 
+    fn compute_base_fee_increase(
+        parent_base_fee: &BigUint,
+        parent_gas_used: &BigUint,
+        parent_target_gas_used: &BigUint,
+    ) -> BigUint {
+        let gas_delta = parent_gas_used - parent_target_gas_used;
+        let fee_delta = cmp::max(
+            (parent_base_fee * gas_delta) / parent_target_gas_used / BASEFEE_MAX_CHANGE_DENOMINATOR,
+            One::one(),
+        );
+        parent_base_fee + fee_delta
+    }
+
+    fn compute_base_fee_decrease(
+        parent_base_fee: &BigUint,
+        parent_gas_used: &BigUint,
+        parent_target_gas_used: &BigUint,
+    ) -> BigUint {
+        let gas_delta = parent_target_gas_used - parent_gas_used;
+        let fee_delta =
+            (parent_base_fee * gas_delta) / parent_target_gas_used / BASEFEE_MAX_CHANGE_DENOMINATOR;
+        parent_base_fee - fee_delta
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,52 @@
-use std::fs;
 use eip_utils::eip1559;
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
+use std::{cmp::Ordering, fs};
 
 const TEST_FILE_PATH: &str = "/Users/abdelhamid/Documents/GitHub/rust/eip-utils/basefee-test.json";
 
 fn main() {
-    let eip1559_service = &eip1559::new();
-    let parent_base_fee: u64 = 1000000000;
-    let parent_gas_used: u64  = 7000000;
+    /*let parent_base_fee: u64 = 1000000000;
+    let parent_gas_used: u64 = 7000000;
     let parent_target_gas_used: u64 = 6000000;
     let parent_base_fee = BigUint::from(parent_base_fee);
-    let parent_gas_used  = BigUint::from(parent_gas_used);
+    let parent_gas_used = BigUint::from(parent_gas_used);
     let parent_target_gas_used = BigUint::from(parent_target_gas_used);
-    let base_fee = eip1559_service.compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
-    println!("base fee: {}", &base_fee);
+    let base_fee =
+        eip1559::compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
+    println!("base fee: {}", &base_fee);*/
 
-    let data= fs::read_to_string(TEST_FILE_PATH);
+    let data = fs::read_to_string(TEST_FILE_PATH);
     let data = match data {
         Ok(c) => c,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
 
-    let test_case = json_to_testcase(&data);
-    println!("test case: {:?}", test_case);
+    let test_cases = json_to_testcase(&data);
+    for case in &test_cases {
+        let parent_base_fee = BigUint::from(case.parent_base_fee);
+        let parent_gas_used = BigUint::from(case.parent_gas_used);
+        let parent_target_gas_used = BigUint::from(case.parent_target_gas_used);
+        let base_fee =
+            eip1559::compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
+        if base_fee.cmp(&BigUint::from(case.expected_base_fee)) != Ordering::Equal {
+            panic!(
+                "invalid base fee: expected {} - got {}",
+                case.expected_base_fee, base_fee
+            );
+        }
+    }
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
-struct TestCase{
+struct TestCase {
     parent_base_fee: u64,
     parent_gas_used: u64,
-    parent_target_gas_used: u64
+    parent_target_gas_used: u64,
+    expected_base_fee: u64,
 }
 
-fn json_to_testcase(data: &str) -> Vec<TestCase>{
+fn json_to_testcase(data: &str) -> Vec<TestCase> {
     let t = serde_json::from_str(data);
     match t {
         Ok(c) => c,
