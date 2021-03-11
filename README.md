5e999d045fb1b23d5fe5531df6324b6623e392d5
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-03-11 14:15:43 +0100

    Implement compute base fee command

--- README.md
+++ README.md
@@ -18,5 +18,37 @@ cargo install --path=eip-utils eip-utils
 ### Usage
 
 ```console
-eip-utils
+eip-utils --help
+```
+
+### Commands
+
+#### EIP-1559
+
+##### eip1559-compute-basefee
+
+```
+Compute base fee from parent block header
+
+USAGE:
+    eip-utils eip1559-compute-basefee --parent-base-fee <parent-base-fee> --parent-gas-used <parent-gas-used> --parent-target-gas-used <parent-target-gas-used>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+        --parent-base-fee <parent-base-fee>                  The value of the parent block base fee
+        --parent-gas-used <parent-gas-used>                  The value of the parent block gas used
+        --parent-target-gas-used <parent-target-gas-used>
+            The value of the parent block target gas used
+```
+
+```console
+# Help
+eip-utils help eip1559-compute-basefee
+# Example 
+eip-utils eip1559-compute-basefee --parent-base-fee=1000000000 --parent-gas-used=10000000 --parent-target-gas-used=5000000
+# Output
+1125000000
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{load_yaml, App, ArgMatches};
+use eip_utils::eip1559;
+use num_bigint::BigUint;
+use std::{process::exit, str::FromStr};
+
+fn main() {
+    let yaml = load_yaml!("../resources/cli.yaml");
+    let matches = App::from(yaml).get_matches();
+    if let Some(ref matches) = matches.subcommand_matches("eip1559-compute-basefee") {
+        // "$ eip-utils eip1559-compute-basefee" was run
+        eip1559_compute_base_fee(&matches);
+        exit(0);
+    }
+    println!("{}", BANNER);
+}
+
+fn eip1559_compute_base_fee(matches: &ArgMatches) {
+    let parent_base_fee = option_to_big(matches.value_of("parent-base-fee"));
+    let parent_gas_used = option_to_big(matches.value_of("parent-gas-used"));
+    let parent_target_gas_used = option_to_big(matches.value_of("parent-target-gas-used"));
+    let base_fee =
+        eip1559::compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
+    println!("{}", base_fee);
+}
+
+fn option_to_big(value: Option<&str>) -> BigUint {
+    str_to_big(value.unwrap())
+}
+
+fn str_to_big(value: &str) -> BigUint {
+    BigUint::from_str(value).expect("invalid literal value specified, expected valid BigUint value")
+}
+
 const BANNER: &str = r#"
 #▓█████  ██▓ ██▓███      █    ██ ▄▄▄█████▓ ██▓ ██▓      ██████ 
 ▓█   ▀ ▓██▒▓██░  ██▒    ██  ▓██▒▓  ██▒ ▓▒▓██▒▓██▒    ▒██    ▒ 
@@ -10,6 +43,3 @@ const BANNER: &str = r#"
    ░  ░ ░                 ░               ░      ░  ░      ░  
                                                               
 "#;
-fn main() {
-    println!("{}", BANNER);
-}
