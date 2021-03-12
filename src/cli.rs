ac2c134f021ceb3ad7b9c1fe1e22c6f4a4f323b9
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2021-03-12 09:48:11 +0100

    clean up

--- src/cli.rs
+++ src/cli.rs
@@ -2,20 +2,15 @@ use crate::util;
 use clap::ArgMatches;
 use eip_utils::eip1559;
 
-pub struct EIP1559Cmd {}
+pub fn eip1559_compute_base_fee(matches: &ArgMatches) {
+    let parent_base_fee = util::option_to_big(matches.value_of("parent-base-fee"));
+    let parent_gas_used = util::option_to_big(matches.value_of("parent-gas-used"));
+    let parent_target_gas_used = util::option_to_big(matches.value_of("parent-target-gas-used"));
+    let base_fee =
+        eip1559::compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
+    println!("{}", base_fee);
+}
 
-impl EIP1559Cmd {
-    pub fn compute_base_fee(&self, matches: &ArgMatches) {
-        let parent_base_fee = util::option_to_big(matches.value_of("parent-base-fee"));
-        let parent_gas_used = util::option_to_big(matches.value_of("parent-gas-used"));
-        let parent_target_gas_used =
-            util::option_to_big(matches.value_of("parent-target-gas-used"));
-        let base_fee =
-            eip1559::compute_base_fee(&parent_base_fee, &parent_gas_used, &parent_target_gas_used);
-        println!("{}", base_fee);
-    }
-
-    pub fn resources(&self) {
-        println!("https://hackmd.io/@timbeiko/1559-resources");
-    }
+pub fn eip1559_resources() {
+    println!("https://hackmd.io/@timbeiko/1559-resources");
 }
--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,14 @@ mod util;
 fn main() {
     let yaml = load_yaml!("../resources/cli.yaml");
     let matches = App::from(yaml).get_matches();
-    let eip_1559 = cli::EIP1559Cmd {};
     if let Some(ref matches) = matches.subcommand_matches("eip1559-compute-basefee") {
         // "$ eip-utils eip1559-compute-basefee" was run
-        eip_1559.compute_base_fee(&matches);
+        cli::eip1559_compute_base_fee(&matches);
         process::exit(0);
     }
     if let Some(_) = matches.subcommand_matches("eip1559-resources") {
         // "$ eip-utils tx-encode" was run
-        eip_1559.resources();
+        cli::eip1559_resources();
         process::exit(0);
     }
-    if let Some(_) = matches.subcommand_matches("tx-encode") {
-        // "$ eip-utils tx-encode" was run
-        tx_encode();
-        process::exit(0);
-    }
-    println!("{}", BANNER);
 }
-
-fn tx_encode() {
-    println!("encode tx");
-    // https://crates.io/crates/rlp
-    // https://github.com/rust-blockchain/ethereum
-}
-
-const BANNER: &str = r#"
-#▓█████  ██▓ ██▓███      █    ██ ▄▄▄█████▓ ██▓ ██▓      ██████ 
-▓█   ▀ ▓██▒▓██░  ██▒    ██  ▓██▒▓  ██▒ ▓▒▓██▒▓██▒    ▒██    ▒ 
-▒███   ▒██▒▓██░ ██▓▒   ▓██  ▒██░▒ ▓██░ ▒░▒██▒▒██░    ░ ▓██▄   
-▒▓█  ▄ ░██░▒██▄█▓▒ ▒   ▓▓█  ░██░░ ▓██▓ ░ ░██░▒██░      ▒   ██▒
-░▒████▒░██░▒██▒ ░  ░   ▒▒█████▓   ▒██▒ ░ ░██░░██████▒▒██████▒▒
-░░ ▒░ ░░▓  ▒▓▒░ ░  ░   ░▒▓▒ ▒ ▒   ▒ ░░   ░▓  ░ ▒░▓  ░▒ ▒▓▒ ▒ ░
- ░ ░  ░ ▒ ░░▒ ░        ░░▒░ ░ ░     ░     ▒ ░░ ░ ▒  ░░ ░▒  ░ ░
-   ░    ▒ ░░░           ░░░ ░ ░   ░       ▒ ░  ░ ░   ░  ░  ░  
-   ░  ░ ░                 ░               ░      ░  ░      ░  
-                                                              
-"#;
